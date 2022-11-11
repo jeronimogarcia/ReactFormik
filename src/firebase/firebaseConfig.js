597c06f90c22ac9f550646c18be95d81d5067e63
jeronimogarcia <jero_garcia23@hotmail.com>
2022-11-11 06:17:53 -0300

    First Commit

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6poRdvVWvq2NucAMT_VztAUX-xS7Pb3M",
+  authDomain: "reactformik-dae4b.firebaseapp.com",
+  projectId: "reactformik-dae4b",
+  storageBucket: "reactformik-dae4b.appspot.com",
+  messagingSenderId: "51682393261",
+  appId: "1:51682393261:web:3c27d05069e7cab42e1a24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
